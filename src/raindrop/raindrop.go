970f870e7fa6f97b94d4e85f15f4fd7945a9f2dd
Jay Panda <jaykpanda@gmail.com>
2017-03-27 11:12:13 +0000

    Fixed raindrop program

--- src/raindrop/raindrop.go
+++ src/raindrop/raindrop.go
@@ -1,19 +1,19 @@
-/* Convert a number to a string, the contents of which depend on the number's factors.
+/*Convert a number to a string, the contents of which depend on the number's factors.
 
 If the number has 3 as a factor, output 'Pling'.
 If the number has 5 as a factor, output 'Plang'.
 If the number has 7 as a factor, output 'Plong'.
-If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
-*/
+If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.*/
 package main
 
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	arg := int(os.Args[1])
+	arg, _ := strconv.Atoi(os.Args[1])
 	s := ""
 	for i := 1; i <= arg; i++ {
 		if arg%i == 0 {
@@ -30,7 +30,7 @@ func main() {
 	}
 	if s == "" {
 		fmt.Println("The raindrop string for the number input is :", arg)
-		exit()
+		os.Exit(0)
 	}
 	fmt.Println("The raindrop string for the number input is :", s)
 }
