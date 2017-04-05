39966efea34782059df55c82f0a27ba516f33444
jaykpanda <jaykpanda@gmail.com>
2017-04-05 21:30:46 +0530

    Create sieve.go
    5- Created sieve.go

---
+++ src/sieve/sieve.go
@@ -0,0 +1,41 @@
+/*Use the Sieve of Eratosthenes to find all the primes from 2 up to a given number.
+The Sieve of Eratosthenes is a simple, ancient algorithm for finding all prime numbers up to any given limit.
+It does so by iteratively marking as composite (i.e. not prime) the multiples of each prime, starting with the multiples of 2. */
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	no, _ := strconv.Atoi(os.Args[1])
+	div := 0
+	var seive [1000]int
+	for i := 2; i <= no; i++ {
+		seive[i] = i
+	}
+	for j := 2; j <= no; j++ {
+		if seive[j] != 0 {
+			for k := 2; k < j; k++ {
+				if (seive[j] % k) == 0 {
+					div = div + 1
+				}
+			}
+		}
+		if div == 0 {
+			for n := 2 * j; n <= no; n = n + j {
+				seive[n] = 0
+			}
+		}
+		div = 0
+	}
+	fmt.Println("All the prime nos upto", no, "are:")
+	for i := 2; i <= no; i++ {
+		if seive[i] != 0 {
+			fmt.Print(i, " ")
+		}
+	}
+}
